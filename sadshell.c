1c9aec69fb56226aae33b03af8a682cd1de7e83d
jensenli049 <jensenli049@gmail.com>
2017-11-22 15:29:26 -0500

    added sadshell files

---
+++ sadshell.c
@@ -0,0 +1,19 @@
+#include "sadshell.h"
+
+int main( int argc, char *argv[] ){
+  while(1){
+    printf("$ad$hell$ ");
+    char input[200];// = (char*)malloc(200*sizeof(char));
+    fgets(input, sizeof(input), stdin);
+    //printf("input: %s", input);
+
+    
+  }
+  
+}
+
+void ParseCmd(){
+  
+}
+
+void
---
+++ sadshell.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
